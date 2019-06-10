e3c30eccac85a8dd0158ef70e920bb79e16e7022
Muxall <moiot@muxall.com>
2019-06-10 13:52:16 -0500

    Implemented Nodes and Links Save and SaveAs

--- Magui/MainWindow.xaml.vb
+++ Magui/MainWindow.xaml.vb
@@ -1,7 +1,106 @@
 ﻿
+Imports System.IO
 Imports System.Windows.Controls.Primitives
 
 Public Class MainWindow
+
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        If Not (Directory.Exists(defaultDir)) Then
+            Directory.CreateDirectory(defaultDir)
+        End If
+
+    End Sub
+
+    Private Sub Open_File(ByVal sender As Object, ByVal e As RoutedEventArgs)
+
+        ' Configure open file dialog box
+        Dim dlg As New Microsoft.Win32.OpenFileDialog()
+        dlg.InitialDirectory = defaultDir
+        dlg.Filter = defaultFileFilter
+
+        ' Show open file dialog box
+        Dim result? As Boolean = dlg.ShowDialog(Me)
+
+        ' Process open file dialog box results
+        If result = True Then
+            ' Open document
+            Dim filename As String = dlg.FileName
+            Debug.WriteLine("Open File called: " & filename)
+            configFile = filename
+        End If
+
+    End Sub
+
+    Private Sub Save_File(ByVal sender As Object, ByVal e As RoutedEventArgs)
+
+        ' Configure save file dialog box
+        If configFile Is Nothing Then
+            SaveAsTestbed(defaultConfigFile)
+        Else
+            SaveTestbed(configFile)
+        End If
+
+    End Sub
+
+    Private Sub SaveAs_File(ByVal sender As Object, ByVal e As RoutedEventArgs)
+
+        If configFile Is Nothing Then
+            SaveAsTestbed(defaultConfigFile)   'Default configFile name.
+        Else
+            SaveAsTestbed(configFile)
+        End If
+
+    End Sub
+
+    Private Sub SaveAsTestbed(ByVal strFile As String)
+
+        ' Configure saveAs file dialog box
+        Dim dlg As New Microsoft.Win32.SaveFileDialog()
+        dlg.FileName = strFile ' Default file name
+        dlg.InitialDirectory = defaultDir
+        dlg.Filter = defaultFileFilter
+
+        ' Show save file dialog box
+        Dim result? As Boolean = dlg.ShowDialog(Me)
+
+        ' Process save file dialog box results
+        If result = True Then
+            Dim filename As String = dlg.FileName
+            SaveTestbed(filename)
+            configFile = filename
+        End If
+    End Sub
+
+    Private Sub SaveTestbed(ByVal strFile As String)
+        ' Save document
+        Using sw As StreamWriter = New StreamWriter(strFile)
+            If colNodes.Count > 0 Then
+                For Each node As ClassNode In colNodes
+                    sw.WriteLine("NODE_START")
+                    sw.WriteLine("nodeName = " & node.Name)
+                    sw.WriteLine("nodeType = " & node.nodeType)
+                    Dim myLocation As Point = node.GetLocation()
+                    sw.WriteLine("left = " & myLocation.X)
+                    sw.WriteLine("top = " & myLocation.Y)
+                    sw.WriteLine("NODE_END")
+                Next
+            End If
+            If colLinks.Count > 0 Then
+                For Each link As Link In colLinks
+                    sw.WriteLine("LINK_START")
+                    sw.WriteLine("linkName = " & link.Name)
+                    sw.WriteLine("linkType = " & link.linkType)
+                    sw.WriteLine("nodeA = " & link.nodeA.Name)
+                    sw.WriteLine("nodeB = " & link.nodeB.Name)
+                    sw.WriteLine("LINK_END")
+                Next
+            End If
+            sw.Close()
+        End Using
+    End Sub
 
     Private Sub Add_Router(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
         Dim idx As Integer
@@ -14,6 +113,7 @@ Public Class MainWindow
         idx = GetAvailableNodeIndex()
         Router.nodeIdx = idx                    'update the default index 0 to new index
         Router.Name = "Router_" & idx           'give the Router a unique name.
+        Router.nodeType = "CiscoRouterNode"     'give the Router a type.
 
         Canvas.SetZIndex(Router, 10)            'Move the Router image to the foreground.
 
@@ -68,6 +168,7 @@ Public Class MainWindow
         idx = GetAvailableNodeIndex()           'Get next available node index
         Eswitch.nodeIdx = idx                   'update the default index 0 to new index
         Eswitch.Name = "Eswitch_" & idx         'give the Eswitch a unique name.
+        Eswitch.nodeType = "CiscoSwitchNode"    'give the Eswitch a type.
 
         Canvas.SetZIndex(Eswitch, 10)            'Move the Eswitch image to the foreground.
 
@@ -85,6 +186,7 @@ Public Class MainWindow
         idx = GetAvailableNodeIndex()       'Get next available node index
         Xena.nodeIdx = idx                  'update the default index 0 to new index
         Xena.Name = "Xena_" & idx           'give the Xena a unique name.
+        Xena.nodeType = "XenaTestSet"       'give the TestSet a type
 
         Canvas.SetZIndex(Xena, 10)          'Move the Xena image to the foreground.
 
--- Magui/ModGlobals.vb
+++ Magui/ModGlobals.vb
@@ -3,4 +3,8 @@
     Public colLinks As New Collection
     Public MaxNodes As Integer = 1000
     Public MaxLinks As Integer = 4000
+    Public defaultDir = (My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\magui")
+    Public defaultFileFilter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+    Public configFile As String
+    Public defaultConfigFile As String = "MaguiConfig.txt"
 End Module
